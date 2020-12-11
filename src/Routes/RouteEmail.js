2f7a9b99d12fdcf697e7e6dd5daaeb15271b3247
jtapiaz <jjtapias20@misena.edu.co>
2020-12-11 17:19:00 -0500

    EnvioCorreo

--- src/Routes/RouteEmail.js
+++ src/Routes/RouteEmail.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 //..............Import the controllers...................................
-const emailController = require("../controllers/contactSupport");
+const emailController = require('../controllers/contactSupport');
 
 
 router.route("/sendEmail").post(emailController.soporte_create);
